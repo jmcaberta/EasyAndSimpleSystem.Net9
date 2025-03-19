5a5b73de14bc59952837903a2d461f3f3d221be1
Juan Miguel <caber_t@hotmail.com>
2025-03-19 22:53:28 +0100

    Created the frontend layout.

--- FrontEaS/system_vue/src/App.vue
+++ FrontEaS/system_vue/src/App.vue
@@ -1,30 +1,81 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <v-layout class="rounded rounded-md border">
+    <v-app-bar title="Easy&Simple"></v-app-bar>
+
+    <v-navigation-drawer expand-on-hover rail>
+      <v-list>
+        <v-list-item 
+        title="Juan Miguel Caberta" 
+        prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg"
+        subtitle="caber_t@hotmail.com">          
+        </v-list-item>
+        <v-divider></v-divider>
+        <v-list density="compact" nav>
+          <v-list-item prepend-icon="mdi-home-city" title="Home" value="home">
+            
+          </v-list-item> 
+          <v-list-item prepend-icon="mdi-account-group-outline" title="Users" value="users"></v-list-item>         
+        </v-list>
+        
+      </v-list>
+    </v-navigation-drawer>
+    
+    <v-main class="d-flex align-center justify-center" height="200">
+      <v-container>
+        <v-sheet
+          border="dashed md"
+          color="surface-light"
+          height="100"
+          rounded="lg"
+          width="100%"
+        ></v-sheet>
+      </v-container>
+    </v-main>    
+  </v-layout>
+  <v-container>
+  
+    <v-sheet
+      border="dashed md"
+      color="surface-light"
+      height="700"
+      rounded="lg"
+      width="100%"></v-sheet>
+  </v-container>
+  <v-divider></v-divider>
+  <v-footer class="d-flex flex-column" rounded="lg">
+    <div class="d-flex w-90 align-center px-4 py-2">
+      <strong>Get connected with us on social networks!</strong>
+
+      <div class="d-flex ga-2 ms-auto">
+        <v-btn
+          v-for="icon in icons"
+          :key="icon"
+          :icon="icon"
+          size="small"
+          variant="plain"
+        ></v-btn>
+      </div>
+    </div>
+
+    <div class="px-6 py-2 bg-surface-variant text-center w-100 rounded-lg">
+      <strong>Easy&Simple</strong> {{ new Date().getFullYear() }} 
+    </div>
+  </v-footer>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
-}
+<script>
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+export default {
+  name: 'App',
 
-nav a.router-link-exact-active {
-  color: #42b983;
+  data: () => ({
+    icons: [
+        'mdi-facebook',
+        'mdi-twitter',
+        'mdi-linkedin',
+        'mdi-instagram',
+      ],
+  }),
 }
-</style>
+</script>
---
+++ FrontEaS/system_vue/src/plugins/vuetify.js
@@ -0,0 +1,26 @@
+// Styles
+//import Vue from 'vue'
+//import Vuetify from 'vuetify'
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles'
+
+// Vuetify
+import { createVuetify } from 'vuetify'
+
+export default createVuetify(
+  // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
+)
+
+/* Vue.use(Vuetify, {
+  theme: {
+  primary:'#1976d2',
+  secondary:'#424242',
+  accent:'#82b1ff',
+  error:'#ff5252',
+  info:'#2196f3',
+  success:'#4caf50',
+  warning:'#ffc107'
+  },
+  customProperties: true,
+  iconfont: 'md'
+}) */
