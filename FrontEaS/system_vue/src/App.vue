c497dd3366c3cc298d465f3599cd19b420f2360d
Juan Miguel <caber_t@hotmail.com>
2025-04-21 22:14:54 +0200

    Changes in v-list-group and in the data table category

--- FrontEaS/system_vue/src/App.vue
+++ FrontEaS/system_vue/src/App.vue
@@ -1,76 +1,90 @@
-
 <template>
   <v-layout class="rounded rounded-md border">
     <v-app-bar title="Easy&Simple"></v-app-bar>
 
     <v-navigation-drawer expand-on-hover rail>
       <v-list color="primary">
-        <v-list-item 
-        title="Juan Miguel Caberta" 
-        prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg"
-        subtitle="caber_t@hotmail.com">          
+        <v-list-item title="Juan Miguel Caberta" prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg"
+          subtitle="caber_t@hotmail.com">
         </v-list-item>
         <v-divider></v-divider>
         <v-list density="compact" nav>
-          <v-list-item color="primary" prepend-icon="mdi-home-city" title="Home" value="home"  variant="text" style="color: var(--v-theme-primary);">        
-          </v-list-item>                  
-        </v-list> 
+          <v-list-item color="primary" prepend-icon="mdi-home-city" title="Home" value="home" variant="text"
+            style="color: var(--v-theme-primary);">
+          </v-list-item>
+        </v-list>
 
-        <v-list-group prepend-icon="mdi-warehouse" value="store" color="primary" style="color: var(--v-theme-primary);">
+        <v-list-group prepend-icon="mdi-warehouse" :value="drawerGroup === 'Warehouse'"
+          @update:active="drawerGroup = $event ? 'Warehouse' : null" color="primary"
+          style="color: var(--v-theme-primary);">
           <template v-slot:activator="{ props }">
-            <v-list-item v-bind="props" title="Warehouse" color="primary" variant="text" style="color: var(--v-theme-primary);"></v-list-item>
+            <v-list-item v-bind="props" title="Warehouse" color="primary" variant="text"
+              style="color: var(--v-theme-primary);"></v-list-item>
           </template>
-          <v-list-item :to="{ name: 'category'}" title="Category" prepend-icon="mdi-sort" color="primary" variant="text" style="color: var(--v-theme-primary);"></v-list-item>
-          <v-list-item title="Products" prepend-icon="mdi-package-variant-closed" color="primary" variant="text" style="color: var(--v-theme-primary);"></v-list-item>          
-          <v-list-item title="Inventory" prepend-icon="mdi-clipboard-list" color="primary" variant="text" style="color: var(--v-theme-primary);"></v-list-item>
-        </v-list-group>       
+          <v-list-item :to="{ name: 'category' }" title="Category" prepend-icon="mdi-sort" color="primary" variant="text"
+            style="color: var(--v-theme-primary);"></v-list-item>
+          <v-list-item title="Products" prepend-icon="mdi-package-variant-closed" color="primary" variant="text"
+            style="color: var(--v-theme-primary);"></v-list-item>
+          <v-list-item title="Inventory" prepend-icon="mdi-clipboard-list" color="primary" variant="text"
+            style="color: var(--v-theme-primary);"></v-list-item>
+        </v-list-group>
         <v-list-group prepend-icon="mdi-cart" value="purchases" color="primary" style="color: var(--v-theme-primary);">
           <template v-slot:activator="{ props }">
-            <v-list-item v-bind="props" title="Purchases" color="primary" variant="text" style="color: var(--v-theme-primary);"></v-list-item>
+            <v-list-item v-bind="props" title="Purchases" color="primary" variant="text"
+              style="color: var(--v-theme-primary);"></v-list-item>
           </template>
-          <v-list-item title="Suppliers" prepend-icon="mdi-truck" color="primary" variant="text" style="color: var(--v-theme-primary);"></v-list-item>
-          <v-list-item title="Purchase Orders" prepend-icon="mdi-clipboard-text" color="primary" variant="text" style="color: var(--v-theme-primary);"></v-list-item>
-          <v-list-item v-list-item title="Purchase Invoice" prepend-icon="mdi-file-table-outline" color="primary" variant="text" style="color: var(--v-theme-primary);"></v-list-item>
-        </v-list-group>     
-        <v-list-group prepend-icon="mdi-storefront" value="store" color="primary" style="color: var(--v-theme-primary);">
-          <template v-slot:activator="{ props }">
-            <v-list-item v-bind="props" title="Sells" color="primary" variant="text" style="color: var(--v-theme-primary);"></v-list-item>
-          </template>          
-          <v-list-item title="Sells" prepend-icon="mdi-cash-register" color="primary" variant="text" style="color: var(--v-theme-primary);"></v-list-item>
-          <v-list-item title="Clients" prepend-icon="mdi-account-circle" color="primary" variant="text" style="color: var(--v-theme-primary);"></v-list-item>
+          <v-list-item title="Suppliers" prepend-icon="mdi-truck" color="primary" variant="text"
+            style="color: var(--v-theme-primary);"></v-list-item>
+          <v-list-item title="Purchase Orders" prepend-icon="mdi-clipboard-text" color="primary" variant="text"
+            style="color: var(--v-theme-primary);"></v-list-item>
+          <v-list-item v-list-item title="Purchase Invoice" prepend-icon="mdi-file-table-outline" color="primary"
+            variant="text" style="color: var(--v-theme-primary);"></v-list-item>
         </v-list-group>
-        <v-list-group prepend-icon="mdi-text-box-multiple" value="Reports" color="primary" style="color: var(--v-theme-primary);">
+        <v-list-group prepend-icon="mdi-storefront" value="store" color="primary"
+          style="color: var(--v-theme-primary);">
           <template v-slot:activator="{ props }">
-            <v-list-item v-bind="props" title="Reports" color="primary" variant="text" style="color: var(--v-theme-primary);"></v-list-item>
+            <v-list-item v-bind="props" title="Sells" color="primary" variant="text"
+              style="color: var(--v-theme-primary);"></v-list-item>
           </template>
-          <v-list-item title="Sales invoice" prepend-icon="mdi-file-table" color="primary" variant="text" style="color: var(--v-theme-primary);"></v-list-item>
-          <v-list-item title="Purchase invoice" prepend-icon="mdi-file-table-outline" color="primary" variant="text" style="color: var(--v-theme-primary);"></v-list-item>          
+          <v-list-item title="Sells" prepend-icon="mdi-cash-register" color="primary" variant="text"
+            style="color: var(--v-theme-primary);"></v-list-item>
+          <v-list-item title="Clients" prepend-icon="mdi-account-circle" color="primary" variant="text"
+            style="color: var(--v-theme-primary);"></v-list-item>
         </v-list-group>
-        <v-list-group prepend-icon="mdi-fingerprint" value="access" color="primary" style="color: var(--v-theme-primary);">
+        <v-list-group prepend-icon="mdi-text-box-multiple" value="Reports" color="primary"
+          style="color: var(--v-theme-primary);">
           <template v-slot:activator="{ props }">
-            <v-list-item v-bind="props" title="Access" color="primary" variant="text" style="color: var(--v-theme-primary);"></v-list-item>
+            <v-list-item v-bind="props" title="Reports" color="primary" variant="text"
+              style="color: var(--v-theme-primary);"></v-list-item>
           </template>
-          <v-list-item prepend-icon="mdi-account-group-outline" title="Users" value="users" color="primary" variant="text" style="color: var(--v-theme-primary);"></v-list-item>
-          <v-list-item title="Rolls" prepend-icon="mdi-account-convert" color="primary" variant="text" style="color: var(--v-theme-primary);"></v-list-item>          
+          <v-list-item title="Sales invoice" prepend-icon="mdi-file-table" color="primary" variant="text"
+            style="color: var(--v-theme-primary);"></v-list-item>
+          <v-list-item title="Purchase invoice" prepend-icon="mdi-file-table-outline" color="primary" variant="text"
+            style="color: var(--v-theme-primary);"></v-list-item>
+        </v-list-group>
+        <v-list-group prepend-icon="mdi-fingerprint" value="access" color="primary"
+          style="color: var(--v-theme-primary);">
+          <template v-slot:activator="{ props }">
+            <v-list-item v-bind="props" title="Access" color="primary" variant="text"
+              style="color: var(--v-theme-primary);"></v-list-item>
+          </template>
+          <v-list-item prepend-icon="mdi-account-group-outline" title="Users" value="users" color="primary"
+            variant="text" style="color: var(--v-theme-primary);"></v-list-item>
+          <v-list-item title="Rolls" prepend-icon="mdi-account-convert" color="primary" variant="text"
+            style="color: var(--v-theme-primary);"></v-list-item>
         </v-list-group>
       </v-list>
     </v-navigation-drawer>
-    
+
     <v-main class="d-flex align-center justify-center" height="200">
       <v-container>
-        
-        <v-sheet
-          border="dashed md"
-          color="surface-light"
-          height="100"
-          rounded="lg"
-          width="100%"
-        ></v-sheet>
-      </v-container>      
-    </v-main>    
+
+        <v-sheet border="dashed md" color="surface-light" height="100" rounded="lg" width="100%"></v-sheet>
+      </v-container>
+    </v-main>
   </v-layout>
   <v-container>
-    <router-view></router-view>     
+    <router-view></router-view>
   </v-container>
   <v-divider></v-divider>
   <v-footer class="d-flex flex-column" rounded="lg">
@@ -78,19 +92,13 @@
       <strong> Caberta Ramirez </strong>
 
       <div class="d-flex ga-2 ms-auto">
-        <v-btn
-          v-for="icon in icons"
-          :key="icon"
-          :icon="icon"
-          size="medium"
-          variant="plain"
-          style="color: var(--v-theme-primary);"
-        ></v-btn>
+        <v-btn v-for="icon in icons" :key="icon" :icon="icon" size="medium" variant="plain"
+          style="color: var(--v-theme-primary);"></v-btn>
       </div>
     </div>
 
     <div class="px-6 py-2 bg-surface-variant text-center w-100 rounded-lg">
-      <strong>Easy&Simple</strong> {{ new Date().getFullYear() }} 
+      <strong>Easy&Simple</strong> {{ new Date().getFullYear() }}
     </div>
   </v-footer>
 </template>
@@ -103,11 +111,12 @@ export default {
 
   data: () => ({
     icons: [
-        'mdi-facebook',
-        'mdi-twitter',
-        'mdi-linkedin',
-        'mdi-instagram',
-      ],
+      'mdi-facebook',
+      'mdi-twitter',
+      'mdi-linkedin',
+      'mdi-instagram',
+    ],
+    drawerGroup: null,
   }),
 }
 </script>
--- FrontEaS/system_vue/src/components/Category.vue
+++ FrontEaS/system_vue/src/components/Category.vue
@@ -1,178 +1,153 @@
 <template>
-    <v-sheet border rounded>
-      <v-data-table
-        :headers="headers"        
-        :items="books"
-        :search="search"
-      >
-        <template v-slot:top>
-          <v-toolbar flat class="d-flex align-center">
-            <v-toolbar-title>
-              <v-icon color="primary" icon="mdi-storefront-outline" size="x-small" start></v-icon>  
-              Category
-            </v-toolbar-title>
-            <v-spacer></v-spacer>
-            <v-text-field 
-                class="me-2"
-                v-model="search"
-                label="Search category"
-                prepend-inner-icon="mdi-magnify"
-                clearable
-                
-                density="compact"
-                variant="outlined"></v-text-field>
-                <v-spacer></v-spacer>
-            <v-btn
-              class="me-2"
-              prepend-icon="mdi-plus"
-              rounded="lg"
-              text="New Category"
-              border
-              @click="add"
-            ></v-btn>
-          </v-toolbar>
-        </template>
-  
-        <template v-slot:item.title="{ value }">
-          <v-chip :text="value" prepend-icon="mdi-book" label>
-            <template v-slot:prepend>
-              <v-icon color="primary"></v-icon>
-            </template>
-          </v-chip>
-        </template>
-  
-        <template v-slot:item.actions="{ item }">
-          <div class="d-flex ga-2 justify-end">
-            <v-icon color="medium-emphasis" icon="mdi-pencil" size="small" @click="edit(item.id)"></v-icon>
-  
-            <v-icon color="medium-emphasis" icon="mdi-delete" size="small" @click="remove(item.id)"></v-icon>
-          </div>
-        </template>
-  
-        <template v-slot:no-data>
-          <v-btn
-            prepend-icon="mdi-backup-restore"
-            rounded="lg"
-            text="Reset data"
-            variant="text"
-            border
-            @click="reset"
-          ></v-btn>
-        </template>
-      </v-data-table>
-    </v-sheet>
-  
-    <v-dialog v-model="dialog" max-width="500">
-      <v-card
-        :subtitle="`${isEditing ? 'Update' : 'Create'} a Category`"
-        :title="`${isEditing ? 'Edit' : 'Add'} Category`"
-      >
-        <template v-slot:text>
-          <v-row>
-            <v-col cols="12" md="8">
-              <v-text-field v-model="record.title" label="Category Id"></v-text-field>
-            </v-col>
-  
-            <v-col cols="12" md="12">
-              <v-text-field v-model="record.author" label="Name"></v-text-field>
-            </v-col>
-  
-            <v-col cols="12" md="12">
-              <v-text-field v-model="record.genre" label="Description"></v-text-field>
-            </v-col>            
-          </v-row>
-        </template>
-  
-        <v-divider></v-divider>
-  
-        <v-card-actions class="bg-surface-light">
-          <v-btn text="Cancel" variant="plain" @click="dialog = false"></v-btn>
-  
+  <v-sheet border rounded>
+    <v-data-table :headers="headers" :items="categories" :search="search">
+      <template v-slot:top>
+        <v-toolbar flat class="d-flex align-center">
+          <v-toolbar-title>
+            <v-icon color="primary" icon="mdi-storefront-outline" size="x-small" start></v-icon>
+            Category
+          </v-toolbar-title>
           <v-spacer></v-spacer>
-  
-          <v-btn text="Save" @click="save"></v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
-  </template>
-  <script>
-    import { onMounted, ref, shallowRef } from 'vue'
-    import { useDate } from 'vuetify'
-    import axios from 'axios'
-  
-    export default {
-      data () {
-        const adapter = useDate()
-        return {
-          categories:[],
-          adapter,
-          DEFAULT_RECORD: { title: '', author: '', genre: '', year: adapter.getYear(adapter.date()), pages: 1 },
-          books: [],
-          record: { title: '', author: '', genre: '', year: adapter.getYear(adapter.date()), pages: 1 },
-          dialog: false,
-          isEditing: false,
-          headers: [
-            { title: 'Id', key: 'CategoryId', align: 'start' },
-            { title: 'Name', key: 'CategoryName' },
-            { title: 'Description', key: 'CategoryDescription' },
-            { title: 'Is Active', key: 'IsActive', align: 'end' },           
-            { title: 'Actions', key: 'actions', align: 'end', sortable: false },
-          ],
-          search: ''
-        }
-      },
-      mounted () {
-        this.reset()
-      },
-      created(){
-        this.list()
-      },
-      methods: {
-        list() {
-          let me = this;
-          axios.get('api/category/Listing').then(function(response){
-              //console.log(response);
-              me.categories = response.data;
-          }).catch(function(error){
-              console.log(error)
-          });          
-        },
-        add () {
-          this.isEditing = false
-          this.record = { ...this.DEFAULT_RECORD }
-          this.dialog = true
-        },
-        edit (id) {
-          this.isEditing = true
-          const found = this.books.find(book => book.id === id)
-          this.record = { ...found }
-          this.dialog = true
-        },
-        remove (id) {
-          const index = this.books.findIndex(book => book.id === id)
-          this.books.splice(index, 1)
-        },
-        save () {
-          if (this.isEditing) {
-            const index = this.books.findIndex(book => book.id === this.record.id)
-            this.books[index] = { ...this.record }
-          } else {
-            this.record.id = this.books.length + 1
-            this.books.push({ ...this.record })
-          }
-          this.dialog = false
-        },
-        reset () {
-          this.dialog = false
-          this.record = { ...this.DEFAULT_RECORD }
-          this.books = [
-            { id: 1, title: 'To Kill a Mockingbird', author: 'Harper Lee', genre: 'Fiction', year: 1960, pages: 281 },
-            { id: 2, title: '1984', author: 'George Orwell', genre: 'Dystopian', year: 1949, pages: 328 },
-            { id: 3, title: 'The Great Gatsby', author: 'F. Scott Fitzgerald', genre: 'Fiction', year: 1925, pages: 180 },
-            { id: 4, title: 'Sapiens', author: 'Yuval Noah Harari', genre: 'Non-Fiction', year: 2011, pages: 443 },
-            { id: 5, title: 'Dune', author: 'Frank Herbert', genre: 'Sci-Fi', year: 1965, pages: 412 },
-          ]
-        },
-      },
+          <v-text-field class="me-2" v-model="search" label="Search category" prepend-inner-icon="mdi-magnify" clearable
+            density="compact" variant="outlined"></v-text-field>
+          <v-spacer></v-spacer>
+          <v-btn class="me-2" prepend-icon="mdi-plus" rounded="lg" text="New Category" border @click="add"></v-btn>
+        </v-toolbar>
+      </template>
+
+      <!-- <template v-slot:item.title="{ value }">
+        <v-chip :text="value" prepend-icon="mdi-book" label>
+          <template v-slot:prepend>
+            <v-icon color="primary"></v-icon>
+          </template>
+        </v-chip>
+      </template> -->
+
+      <template v-slot:item.actions="{ item }">
+        <div class="d-flex ga-2 justify-end">
+          <v-icon color="medium-emphasis" icon="mdi-pencil" size="small" @click="edit(item.CategoryId)"></v-icon>
+          <v-icon color="medium-emphasis" icon="mdi-delete" size="small" @click="remove(item.CategoryId)"></v-icon>
+        </div>
+      </template>
+
+      <template v-slot:no-data>
+        <v-btn prepend-icon="mdi-backup-restore" rounded="lg" text="Reload from API" variant="text" border
+          @click="list"></v-btn>
+      </template>
+    </v-data-table>
+  </v-sheet>
+
+  <v-dialog v-model="dialog" max-width="500">
+    <v-card :subtitle="`${isEditing ? 'Update' : 'Create'} a Category`"
+      :title="`${isEditing ? 'Edit' : 'Add'} Category`">
+      <template v-slot:text>
+        <v-row>
+          <v-col cols="12" md="8">
+            <v-text-field v-model="record.CategoryId" label="Category Id"></v-text-field>
+          </v-col>
+
+          <v-col cols="12" md="12">
+            <v-text-field v-model="record.CategoryName" label="Name"></v-text-field>
+          </v-col>
+          <v-col cols="12" md="12">
+            <v-text-field v-model="record.CategoryDescription" label="Description"></v-text-field>
+          </v-col>
+          <v-col cols="12" md="12">
+            <v-switch v-model="record.IsActive" label="IsActive"></v-switch>
+          </v-col>
+        </v-row>
+      </template>
+
+      <v-divider></v-divider>
+
+      <v-card-actions class="bg-surface-light">
+        <v-btn text="Cancel" variant="plain" @click="dialog = false"></v-btn>
+        <v-spacer></v-spacer>
+        <v-btn text="Save" @click="save"></v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+<script>
+import { onMounted, ref, shallowRef } from 'vue'
+import { useDate } from 'vuetify'
+import axios from 'axios'
+
+export default {
+  data()
+  {
+    const adapter = useDate()
+    return {
+      categories: [],
+      adapter,
+      DEFAULT_RECORD: { CategoryId: null, CategoryName: '', CategoryDescription: '', IsActive: true },
+      record: { CategoryId: null, CategoryName: '', CategoryDescription: '', IsActive: true },
+      dialog: false,
+      isEditing: false,
+      headers: [
+        { title: 'Id', key: 'categoryId', align: 'start' },
+        { title: 'Name', key: 'categoryName' },
+        { title: 'Description', key: 'categoryDescription', sortable: false },
+        { title: 'Is Active', key: 'isActive', align: 'end' },
+        { title: 'Actions', key: 'actions', align: 'end', sortable: false },
+      ],
+      search: ''
     }
-  </script>
+  },
+  mounted()
+  {
+    this.list()
+  },
+  methods: {
+    list()
+    {
+      let me = this;
+      axios.get('api/Category/Listing').then(function (response)
+      {
+        //console.log(response);
+        me.categories = response.data;
+      }).catch(function (error)
+      {
+        console.log(error)
+      });
+    },
+    add()
+    {
+      this.isEditing = false
+      this.record = { ...this.DEFAULT_RECORD }
+      this.dialog = true
+    },
+    edit(id)
+    {
+      this.isEditing = true
+      const found = this.categories.find(cat => cat.CategoryId === id)
+      this.record = { ...found }
+      this.dialog = true
+    },
+    remove(id)
+    {
+      const index = this.categories.findIndex(cat => cat.CategoryId === id)
+      this.categories.splice(index, 1)
+    },
+    save()
+    {
+      if (this.isEditing)
+      {
+        const index = this.categories.findIndex(cat => cat.CategoryId === this.record.CategoryId)
+        this.categories[index] = { ...this.record }
+      } else
+      {
+        this.record.CategoryId = this.categories.length + 1
+        this.categories.push({ ...this.record })
+      }
+      this.dialog = false
+    },
+    /* reset()
+    {
+      this.dialog = false
+      this.record = { ...this.DEFAULT_RECORD }
+      this.categories = []
+    }, */
+  },
+}
+</script>
